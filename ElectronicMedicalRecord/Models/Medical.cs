98a66becc6b8acd12ec565622d4cc2d06cbb4861
LarrySon786 <richardsbrandon3@gmail.com>
2026-09-18 10:48:57 -0400

    There were some bugs that arose from recent merges. Mostly name spaces and duplicate models. Fixed with this commit

--- ElectronicMedicalRecord/Models/Medical.cs
+++
@@ -1,7 +0,0 @@
-public class Medical
-{
-    public int Id { get; set; }
-    public int PatientId { get; set; }
-    public Patient? Patient { get; set; }
-    public List<string> Medications { get; set; } = new List<string>();
-}
