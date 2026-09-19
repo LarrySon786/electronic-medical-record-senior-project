069cbdcd6789cb308d5074cbd84ba232c0705810
LarrySon786 <richardsbrandon3@gmail.com>
2026-09-19 12:09:21 -0400

    Patient Service hooked up to app patient listings. Patient Dialog hooked up. Added patient data seeder. Seeding data works on clicking database reset

--- ElectronicMedicalRecord/Services/Patient/PatientService.cs
+++ ElectronicMedicalRecord/Services/Patient/PatientService.cs
@@ -55,22 +55,22 @@ public class PatientService
 
 
     // Create patient
-    // || Create a new patient using a patientDTO object 
-    public async Task<Patient> CreatePatient(PatientDto patientDto, ProjectDatabaseConnection? context = null)
+    // || Create a new patient using a patient object 
+    public async Task<Patient> CreatePatient(Patient patient, ProjectDatabaseConnection? context = null)
     {
         return await _context.ExecuteAsync(async db =>
         {
             // Creates a patient entity and assigns Patient Dto data to that entity
             Patient entity = new()
             {
-                FirstName = patientDto.FirstName,
-                MiddleName = patientDto.MiddleName,
-                LastName = patientDto.LastName,
-                DateOfBirth = patientDto.DateOfBirth,
-                PhoneNumber = patientDto.PhoneNumber,
-                Email = patientDto.Email,
-                Address = patientDto.Address,
-                IsDisabled = patientDto.IsDisabled,
+                FirstName = patient.FirstName,
+                MiddleName = patient.MiddleName,
+                LastName = patient.LastName,
+                DateOfBirth = patient.DateOfBirth,
+                PhoneNumber = patient.PhoneNumber,
+                Email = patient.Email,
+                Address = patient.Address,
+                IsDisabled = patient.IsDisabled,
                 MedicalOverview = new()
             };
 
