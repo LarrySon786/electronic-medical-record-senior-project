28707cc84050eb2d30b3f8ad58745d63733e31c8
LarrySon786 <richardsbrandon3@gmail.com>
2026-09-21 12:05:54 -0400

    Finished Authentication and Authorization (with the exception of not having hooked an employee service up yet). Created Data seeding services to seed developer data and test that other services are functioning. Created Controller for login requests. Created CSS for login page. Added to Dev-tools the login info for users. Added [Authorization] for Patients route. Added logout feature to navigation menu.

---
+++ ElectronicMedicalRecord/Migrations/20260921142655_AuthenticationAuthorizationSetup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ElectronicMedicalRecord.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthenticationAuthorizationSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestEmployeeModelDb",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    applicationUserId = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestEmployeeModelDb", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestEmployeeModelDb_AspNetUsers_applicationUserId",
+                        column: x => x.applicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestEmployeeModelDb_applicationUserId",
+                table: "TestEmployeeModelDb",
+                column: "applicationUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestEmployeeModelDb");
+        }
+    }
+}
