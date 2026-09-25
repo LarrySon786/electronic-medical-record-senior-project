6de540eda92d89689de2355a05db2ab438e367fc
LarrySon786 <richardsbrandon3@gmail.com>
2026-09-25 15:56:25 -0400

    Fixing patient listing pages so it used the patient services

--- ElectronicMedicalRecord/Migrations/20260925195439_AdjustingMedicalOverview.Designer.cs
+++ ElectronicMedicalRecord/Migrations/20260925195439_AdjustingMedicalOverview.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ElectronicMedicalRecord.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ElectronicMedicalRecord.Migrations
 {
     [DbContext(typeof(ProjectDatabaseConnection))]
-    partial class ProjectDatabaseConnectionModelSnapshot : ModelSnapshot
+    [Migration("20260925195439_AdjustingMedicalOverview")]
+    partial class AdjustingMedicalOverview
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
